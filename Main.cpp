3952580a8ab0a1222da53000e37def54e302cdc1
Mathieu <mathieu.vigneault.19@gmail.com>
2020-12-13 12:30:45 -0500

    project clion creation

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mathieu(18);
+    cout << "Mathieu est: " << &mathieu << endl;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/CPlus_Course.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlus_Course.dir/Main.cpp.obj"
+  "CPlus_Course.exe"
+  "CPlus_Course.exe.manifest"
+  "CPlus_Course.pdb"
+  "libCPlus_Course.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlus_Course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
